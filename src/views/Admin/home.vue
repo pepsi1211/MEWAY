fb58781e9c1bf22329685c7d0e96eca4821f1c6b
pepsi1211 <715803334@qq.com>
2020-07-17 14:02:46 +0800

    ...

--- src/views/Admin/home.vue
+++ src/views/Admin/home.vue
@@ -33,9 +33,41 @@
             <i class="el-icon-setting"></i>
           </div>
           <div class="entry-box">
-            <div>
-              <i></i>
+            <div class="item">
+              <i class="icon xbtb"></i>
               <span>选班调班</span>
+            </div>
+            <div class="item">
+              <i class="icon kb"></i>
+              <span>课表</span>
+            </div>
+            <div class="item">
+              <i class="icon dmjl"></i>
+              <span>点名记录</span>
+            </div>
+            <div class="item">
+              <i class="icon qkjl"></i>
+              <span>缺课记录</span>
+            </div>
+            <div class="item">
+              <i class="icon xmx"></i>
+              <span>小麦秀</span>
+            </div>
+            <div class="item">
+              <i class="icon zdxy"></i>
+              <span>在读学员</span>
+            </div>
+            <div class="item">
+              <i class="icon qzxy"></i>
+              <span>潜在学员</span>
+            </div>
+            <div class="item">
+              <i class="icon xfyj"></i>
+              <span>续费预警</span>
+            </div>
+            <div class="item">
+              <i class="icon sjqd"></i>
+              <span>收据清单</span>
             </div>
           </div>
         </div>
@@ -49,7 +81,12 @@
             <span>待办事项</span>
             <i class="el-icon-setting"></i>
           </div>
-          <div></div>
+          <div class="todoList">
+            <div class="todoItem">
+              <span class="icon">续费</span>
+              <span>有21位学员需通知续费</span>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -72,7 +109,7 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 24px;
+  box-sizing: border-box;
   .header {
     width: 100%;
     background: #fff;
@@ -81,6 +118,7 @@ export default {
     flex-direction: column;
     border-radius: 5px;
     margin-bottom: 30px;
+    box-sizing: border-box;
     .title {
       width: 100%;
       display: flex;
@@ -132,12 +170,100 @@ export default {
     }
   }
   .section {
-    width: 80vw;
+    width: 100%;
     display: flex;
     align-items: flex-start;
     justify-content: space-between;
+    box-sizing: border-box;
     .left{
       width: 50vw;
+      background: #fff;
+      border-radius: 5px;
+      display: flex;
+      flex-direction: column;
+      padding: 10px 30px;
+      .entry-box{
+        width: 100%;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: flex-start;
+        margin-top: 20px;
+        .item{
+          width: 230px;
+          padding: 20px 0;
+          display: flex;
+          align-items: center;
+          .icon{
+            display: inline-block;
+            width: 42px;
+            height: 42px;
+            background: url(../../../public/img/gift.png) no-repeat 50%;
+            background-size: 100%;
+            background-position-x: 0;
+            margin-right: 20px;
+            // 选班调班
+            &.xbtb{
+              background-position-y: -42px;
+            }
+            // 课表
+            &.kb{
+              background-position-y: -126px;
+            }
+            // 点名记录
+            &.dmjl{
+              background-position-y: -214px;
+            }
+            // 缺课记录
+            &.qkjl{
+              background-position-y: -299px;
+            }
+            // 小麦秀
+            &.xmx{
+              background-position-y: -805px;
+            }
+            // 在读学员
+            &.zdxy{
+              background-position-y: -887px;
+            }
+            // 潜在学员
+            &.qzxy{
+              background-position-y: -973px;
+            }
+            // 续费预警
+            &.xfyj{
+              background-position-y: -1058px;
+            }
+            // 收据清单
+            &.sjqd{
+              background-position-y: -467px;
+            }
+            // 欠费收据
+            &.qfsj{
+              background-position-y: -549px;
+            }
+            // 报名续费
+            &.bmxf{
+              background-position-y: -636px;
+            }
+            // 快速收款
+            &.kssk{
+              background-position-y: -467px;
+            }
+            // 工资结算
+            &.gzjs{
+              background-position-y: -720px;
+            }
+            // 充值管理
+            &.czgl{
+              background-position-y: -467px;
+            }
+          }
+        }
+      }
+    }
+    .right{
+      width: 26vw;
       background: #fff;
       border-radius: 5px;
       display: flex;
